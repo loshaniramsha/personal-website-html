0187aa326e18bc5d83fb293b9a7cf835e8605a7e
loshaniramsha <loshanikottagoda@gmail.com>
2024-04-23 11:42:19 +0530

    add new html and styles assignment section

---
+++ assets/scripts.js
@@ -0,0 +1,14 @@
+const box = document.querySelectorAll(".assignment-box");
+const colorBox = document.querySelectorAll(".color-box");
+
+box.forEach((box, index) => {
+    box.addEventListener("mouseover", () => {
+        colorBox[index].style.animation = "loshani 1s ease-out forwards";
+    });
+    box.addEventListener("mouseleave", () => {
+        colorBox[index].style.animation = "none";
+    });
+});
+
+
+
