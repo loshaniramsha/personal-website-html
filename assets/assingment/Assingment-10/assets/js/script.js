9657c264c65450544a660c07d192a5d627454c40
loshaniramsha <loshanikottagoda@gmail.com>
2024-06-03 22:30:21 +0530

    add assingment 10

---
+++ assets/assingment/Assingment-10/assets/js/script.js
@@ -0,0 +1,23 @@
+const display=document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value+=input;
+}
+
+function clearDisplay() {
+    display.value="";
+    
+}
+function calculate() {
+    try {
+        display.value=eval(display.value);
+    }
+    catch (error) {
+        display.value = "Error";
+    }
+}
+function clearDisplayOneByOne() {
+    display.value = display.value.slice(0, -1); // Remove the last character from the display value
+}
+
+
